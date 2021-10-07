a6c9e34d5226b2013e2284eb1559f330d3dcf02b
Astha Nayak <nayakastha2911@gmail.com>
2021-10-07 14:14:09 +0530

    erasing previous repository

--- lib/utils/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color primaryColor = Color(0xFF49233C);
-Color lightPlumColor = Color(0xFF4D2740);
-Color dateColor = Color(0xFF66455B);
-Color whiteColor = Colors.white;
-Color searchColor = Color(0xFF2A0A1D);
-Color brownColor = Color(0xFFD39977);
-Color plumColor = Color(0xFF41162F);

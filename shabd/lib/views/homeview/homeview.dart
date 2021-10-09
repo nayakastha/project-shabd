3af2caea3162d1b7408f4144c3c3d65c4692bd6a
Astha Nayak <nayakastha2911@gmail.com>
2021-10-10 01:00:23 +0530

    auth starter code implemented

---
+++ shabd/lib/views/homeview/homeview.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shabd/views/homeview/homecontroller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}

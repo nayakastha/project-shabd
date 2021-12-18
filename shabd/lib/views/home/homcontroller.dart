e67031999b62a091e805cc91900d7ef7de4bc1c4
Astha Nayak <nayakastha2911@gmail.com>
2021-12-18 11:02:42 +0530

    homecontroller and page

--- shabd/lib/views/home/homcontroller.dart
+++ shabd/lib/views/home/homcontroller.dart
@@ -1,3 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  late RxString _address;
+
+  Future<String> getCurrentAddress() async {
+    Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.best,
+            forceAndroidLocationManager: true)
+        .then((Position position) async {
+      List<Placemark> placemark =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
+      Placemark? place = placemark[0];
+      _address.value =
+          "${place.locality}, ${place.postalCode}, ${place.country}";
+    }).catchError((e) {
+      debugPrint(e.toString());
+    });
+    return _address.value;
+  }
+
+  void storeAddress() {
+    GetStorage().write('address', _address.value);
+  }
+}
--- shabd/lib/views/home/homescreen.dart
+++ shabd/lib/views/home/homescreen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shabd/utils/theme.dart';
 import 'package:shabd/views/home/homcontroller.dart';
+import 'package:shabd/views/home/widgets/newtilelist.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kBgColour,
+        title: Text(
+          'SHABD',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        elevation: 4,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.settings_rounded,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: const [
+          NewsTileList(),
+        ],
+      ),
+    );
   }
 }
